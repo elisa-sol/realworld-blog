b22041ef14ab262d2e33cf4cf26b02f37f64bb51
elisa-sol <elizaveta-2002@bk.ru>
2024-09-13 19:44:59 +0300

    commit

--- src/components/editProfile/editProfile.jsx
+++ src/components/editProfile/editProfile.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
 import classes from './editProfile.module.scss';
+import isEmail from 'validator/lib/isEmail.js';
 
 function EditProfile() {
   return (
     <div className={classes['edit-profile']}>
-      <div className={classes.container}></div>
+      <div className={classes.header}> Edit Profile </div>
+      <form className={classes.container}>
+        <div className={classes.name}>
+          Username
+          <input className={classes.username} id="username" type="text" placeholder="" />
+        </div>
+
+        <div className={classes.name}>
+          Email address
+          <input className={classes.email} id="email" type="text" placeholder="" />
+        </div>
+
+        <div className={classes.name}>
+          New password
+          <input className={classes.password} id="password" type="password" placeholder="New password" />
+        </div>
+
+        <div className={classes.name}>
+          Avatar image (url)
+          <input className={classes.password} id="password" type="password" placeholder="Avatar image" />
+        </div>
+      </form>
     </div>
   );
 }
