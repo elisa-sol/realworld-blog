cf7e6026695fa96321bcb9c6852a4cfa11be7dbb
elisa-sol <elizaveta-2002@bk.ru>
2024-09-17 16:13:58 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <App style={{ backgroundColor: 'grey' }} />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
