d2441297d301b19620fe58d7cde42ccde8a28a96
elisa-sol <elizaveta-2002@bk.ru>
2024-09-17 16:18:43 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import classes from './app.module.scss';
 import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App style={{ backgroundColor: 'grey' }} />
+        <App className={classes.app} />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
